3aebb936e237b80bdedb5227afbed976bc282dd6
19ngaddam <39496206+19ngaddam@users.noreply.github.com>
2018-06-13 08:28:35 -0400

    Update Computer.java

--- Computer.java
+++ Computer.java
@@ -49,21 +49,52 @@ public class Computer {
 	private void randomPlacement(Ship s) throws Exception{
 		int length=s.getID();
 
-		double dir;
+		int dir;
 		int xstarting;
 		int ystarting;
 
 		boolean allclear=true;
 		do{
 			allclear=true;
-			dir=Math.random();
-			xstarting=(int)Math.random()*9+1;
-			ystarting=(int)Math.random()*9+1;
+			dir=(int)Math.round(Math.random()*4);
+			xstarting=(int) Math.round(Math.random()*9);
+			ystarting=(int)Math.round(Math.random()*9);
 
 
 
-			if(dir<0.5){
+			if(dir==1){
 
+				if(ystarting-length<0){
+					allclear=false;
+				}
+				else{
+
+					for(int i=ystarting; i>ystarting-length;i--){
+
+
+						if(compboard.getPoint(xstarting,i).getID()!=0){
+							allclear=false;
+						}
+					}
+				}
+			}
+			else if(dir==2){
+				if(xstarting+length>10){
+					allclear=false;
+				}
+				else{
+
+					for(int i=xstarting; i<xstarting+length;i++)
+
+
+						if(compboard.getPoint(i,ystarting).getID()!=0){
+							allclear=false;
+						}
+
+				}
+
+			}
+			else if(dir==3){
 				if(ystarting+length>10){
 					allclear=false;
 				}
@@ -77,14 +108,16 @@ public class Computer {
 						}
 					}
 				}
+				
+				
 			}
 			else{
-				if(xstarting+length>10){
+				if(xstarting-length<0){
 					allclear=false;
 				}
 				else{
 
-					for(int i=xstarting; i<xstarting+length;i++)
+					for(int i=xstarting; i<xstarting-length;i--)
 
 
 						if(compboard.getPoint(i,ystarting).getID()!=0){
@@ -92,122 +125,24 @@ public class Computer {
 						}
 
 				}
-
 			}
 
 
 		}while(allclear==false);
 		
-		if(dir<=0.5){
-			compboard.placeShip(xstarting, ystarting, xstarting, ystarting+length, s);
+		if(dir==1){
+			compboard.placeShip(xstarting, ystarting, xstarting, ystarting-length, s);
 		}
-		else{
+		else if(dir==2){
 
 			compboard.placeShip(xstarting, ystarting, xstarting+length, ystarting, s);
 		}
-	}
-
-
-	
-
-	public Grid getGrid(){
-		return compboard;
-	}
-
-
-
-}; i<5;i++){
-
-			ships[i]=new Ship(i+1);
-		}
-
-
-		placeAllShips();
-
-
-
-
-
-
-
-
-	}
-
-
-
-
-	private void placeAllShips() throws Exception{
-
-		int i=0;	
-
-		while(i<5){
-
-
-			randomPlacement(ships[i]);
-
-			i++;
-
-		}
-	}
-
-	private void randomPlacement(Ship s) throws Exception{
-		int length=s.getID();
-
-		double dir;
-		int xstarting;
-		int ystarting;
-
-		boolean allclear=true;
-		do{
-
-			dir=Math.random();
-			xstarting=(int)Math.random()*9+1;
-			ystarting=(int)Math.random()*9+1;
-
-
-
-			if(dir<=0.5){
-
-				if(ystarting+length>10){
-					allclear=false;
-				}
-				else{
-
-					for(int i=ystarting; i<ystarting+length;i++)
-
-
-						if(compboard.getPoint(xstarting,i).getID()!=0){
-							allclear=false;
-						}
-
-				}
-			}
-			else{
-				if(xstarting+length>10){
-					allclear=false;
-				}
-				else{
-
-					for(int i=xstarting; i<xstarting+length;i++)
-
-
-						if(compboard.getPoint(i,ystarting).getID()!=0){
-							allclear=false;
-						}
-
-				}
-
-			}
-
-
-		}while(allclear==false);
-		
-		if(dir<=0.5){
+		if(dir==3){
 			compboard.placeShip(xstarting, ystarting, xstarting, ystarting+length, s);
 		}
-		else{
+		else {
 
-			compboard.placeShip(xstarting, ystarting, xstarting+length, ystarting, s);
+			compboard.placeShip(xstarting, ystarting, xstarting-length, ystarting, s);
 		}
 	}
 
