d38f0a46f17e0b80e3b99fa4babeda3d7d6364ec
19ngaddam <39496206+19ngaddam@users.noreply.github.com>
2018-06-15 10:17:50 -0400

    Update Ship.java

--- Ship.java
+++ Ship.java
@@ -1,82 +1,91 @@
+
+
 public class Ship {
 
-	
+
 	private String name;
-	
+
 	private int ID;//serves as ID the ship and the length
-	
 
-	
-	
-	
+
+
+
+
 	private int lives;
-	
-
-	
 
 
-	
-	
+
+
+
+
+
 	public Ship(int id){
-		
-		ID=id;
-		
-		switch(ID){
-		
-		case 2: name= "Destroyer";
-		
-		case 3: name="Submarine";
-		
-	
-		
-		case 4: name="Battleship";
-		
-		case 5: name="Carrier";
-		
-		case 6: name="Ultimate Patrol Boat";
-		
-		
-		}
-		
 
-			
+		ID=id;
+
+		if(id==2){
+
+			name= "Destroyer";
+		}
+		else if(id==3){
+			name="Submarine";
+
+		}
+
+		else if(id==4){ 
+			name="Battleship";
+		}
+		else if(id==5){ 
+			name="Carrier";
+		}
+		else if(id==6){
+			name="Ultimate Patrol Boat";
+
+
+		}
+
+
+
 		lives=ID;
 	}
-	
-public String getName(){
-		
-		
-		
+
+	public String getName(){
+
+
+
 		return name;
 	}
-	
+
 	public boolean sunk(){
-		
+
 		if (lives<=0)
-		
+
 			return true;
-		
+
 		else
 			return false;
-		
+
 	}
-	
-public void hitShip(){
-	
-	
-	lives--;
-	
-}
-public int getID(){
-	
-	return ID;
-	
-	
-}
-	
-	
-	
-	
-	
-	
+
+	public void hitShip(){
+
+
+		lives--;
+
+	}
+	public int getID(){
+
+		return ID;
+
+
+	}
+
+	public int getlives(){
+		return lives;
+
+	}
+
+
+
+
 }
