d6e35a6a4f25b4d1f019fe7ca4b2466c8fca461a
19ngaddam <39496206+19ngaddam@users.noreply.github.com>
2018-06-17 15:28:15 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -2,8 +2,8 @@ import BreezySwing.*;
 import javax.swing.*;
 public class Player {
 
-	
-	
+
+
 	Grid playerboard;
 	Ship[] ships; 
 
@@ -19,35 +19,50 @@ public class Player {
 		}
 		playerboard=new Grid();
 	}
-	
-	
+
+
 	public void attack(int x, int y) throws Exception{
-		
+
 		playerboard.attack(x, y);
 		int i=playerboard.getPoint(x, y).getID();
 		ships[i-1].hitShip();
 		if(ships[i-1].sunk()==true){
-			
+
 			Board.sGUI.messageBox(ships[i-1].getName()+" has been sunk!");
 		}
-		
-		
+
+
 	}
-	 
-	
-	
+	public void attack(Point p) throws Exception{
+		int x=p.getX();
+		int y=p.getY();
+		playerboard.attack(x, y);
+		int i=playerboard.getPoint(x, y).getID();
+		if(playerboard.getPoint(x, y).getID() != 0) {
+			this.getShip(i).hitShip();
+			if(this.getShip(i).sunk()==true){
+
+				Board.sGUI.messageBox("Your " + this.getShip(i).getName()+" has been sunk!");
+			}
+		}
+
+
+
+	}
+
+
 	public Grid getGrid(){
 		return playerboard;
 	}
-	
+
 	public Ship getShip(int s){
-	
+
 		return ships[s-2];
-		
+
 	}
 	public boolean gg() {
-		
-		
+
+
 		boolean gg=true;
 		for(int i=0; i<5;i++) {
 			if(ships[i].sunk()==false) {
@@ -55,15 +70,15 @@ public class Player {
 			}
 		}
 		return gg;
-		
+
 	}
 	public boolean testSunk(int x, int y) {
-		
-		
-		
-		
+
+
+
+
 		int id=playerboard.getPoint(x, y).getID();
 		return ships[id-2].sunk();
 	}
-	
+
 }
