2007be6f523ce3cefd737517044dc4e4e78d33e6
19ngaddam <39496206+19ngaddam@users.noreply.github.com>
2018-06-17 15:27:58 -0400

    Update Grid.java

--- Grid.java
+++ Grid.java
@@ -27,7 +27,7 @@ public class Grid {
 
 
 	public void placeShip(int row1, int column1, int row2, int column2, Ship s) throws Exception{
-		if(row1>LENGTH || row1<0 ||row2>LENGTH || row2<0 ||column1>LENGTH ||column1<0 || column2>LENGTH||column2<0){
+		if(row1>=LENGTH || row1<0 ||row2>=LENGTH || row2<0 ||column1>=LENGTH ||column1<0 || column2>=LENGTH||column2<0){
 
 			throw new Exception("Enter valid coordinates to place ship");
 
@@ -42,9 +42,16 @@ public class Grid {
 				throw new Exception(s.getName()+" must be "+ s.getID()+" units long");
 
 			}
-
+			for(int i=Math.min(column1, column2);i<=Math.max(column1, column2);i++) {
+				if(grid[row1][i].getID()!=0) {
+					throw new Exception("There is already a ship placed here!!");
+				}
+				
+				
+				
+			}
 			for(int i=Math.min(column1, column2);i<=Math.max(column1, column2);i++){
-
+				
 				grid[row1][i].addShip(s.getID());
 
 			}
@@ -60,7 +67,12 @@ public class Grid {
 				throw new Exception(s.getName()+" must be "+ s.getID()+" units long");
 
 			}
+			for(int i=Math.min(row1, row2);i<=Math.max(row1, row2);i++){
 
+				if(grid[i][column1].getID()!=0) {
+					throw new Exception("Ship has already been placed here!!");
+				}
+			}
 			for(int i=Math.min(row1, row2);i<=Math.max(row1, row2);i++){
 
 				grid[i][column1].addShip(s.getID());
@@ -129,12 +141,25 @@ public class Grid {
 
 
 	}
+	public void attack(Point p) throws Exception{
+
+		int x=p.getX();
+		int y=p.getY();
+
+		grid[x][y].attack();
+
+
+
+
+	}
 
 	public Point getPoint(int x, int y){
 
 		return grid[x][y];
 
 	}
-
+	public Point[][] getAllPoints(){
+		return grid;
+	}
 
 }
